8ec7811b46e560598fb20393d9c19ba5b52fdd3b
paresh <mystylepkh@gmail.com>
2021-12-22 12:10:20 +0530

    Secret key Removed

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-vr#uy$bi&r9+39v=70mdk^29(mw8-@^444+&e616m20awl)yw!'
-
+#SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production#!
 #DEBUG = True
 
